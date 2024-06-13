f86c746bdad0c2f5bd5866d08f69cd5230bfbd85
d3basmi1a <d3basmi1a@gmail.com>
2024-06-13 23:33:27 +0530

    del

--- CashFlow/index.js
+++ CashFlow/index.js
@@ -1,44 +1,53 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+require('dotenv').config();
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-const app= express()
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+const app = express();
+const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/CASHFLOW')
-var db = mongoose.connection
-db.on('error', ()=> console.log("Error in connecting to the Database"))
-db.once('open', () => console.log("Connected to Database"))
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/add", (req,res) =>{
-    var category_select = req.body.category_select
-    var amount_input= req.body.amount_input
-    var info = req.body.info
-    var date_input = req.body.date_input
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-    var data={
+var db = mongoose.connection;
+db.on('error', (error) => console.error("Error in connecting to the Database:", error));
+db.once('open', () => console.log("Connected to Database"));
+
+// Routes
+app.post("/add", (req, res) => {
+    var { category_select, amount_input, info, date_input } = req.body;
+
+    var data = {
         "Category": category_select,
-        "Amount" : amount_input,
+        "Amount": amount_input,
         "Info": info,
         "Date": date_input
-    }
-    db.collection('users').insertOne(data, (err,collection) => {
-        if(err){
-            throw err;
+    };
+
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
+            console.error("Error inserting record:", err);
+            return res.status(500).send("Error inserting record");
         }
-        console.log("Record Inserted Successfully")
+        console.log("Record Inserted Successfully");
+        res.status(200).send("Record Inserted Successfully");
+    });
+});
 
-    })
-})
-app.get("/",(req,res) =>{
-    res.set({
-        "Allow-access-Allow-Origin":'*'
-    })
-    return res.redirect('index.html')
-}).listen(5000)
+app.get("/", (req, res) => {
+    res.set({ "Allow-access-Allow-Origin": '*' });
+    return res.redirect('index.html');
+});
 
-console.log("Listening on port 5000")
+// Start Server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
